114b8c14e76f8abd2f5813469c2705f91f62d1e8
Alicia Beltran <aliceresponde@gmail.com>
2014-12-27 21:51:18 -0500

    cabecera del contrato

--- app/src/main/java/com/nes/example/android/ContratoBD.java
+++ app/src/main/java/com/nes/example/android/ContratoBD.java
@@ -2,6 +2,9 @@ package com.nes.example.android;
 
 /**
  * Created by user on 20/12/2014.
+ * Este codigo contirne la informacion de las tablas que se encuentran en Parse. 
+ * Con el fin de poder acceder a los nombres de los campos de este, a partir de constantes q
+ * Referencian sus nombres tal como esta en Parse
  */
 public class ContratoBD {
 
